29a2f8676ad77d2aca5b6c6d63854295f87161e5
Naren_karthick <152664918+Naren-Karthick@users.noreply.github.com>
2024-10-06 17:56:05 +0530

    Update project.py

--- class12project/project.py
+++ class12project/project.py
@@ -8,7 +8,7 @@ import docx
 tk.set_appearance_mode("dark")
 tk.set_default_color_theme("blue")
 con = mysql.connector.connect(
-    host="localhost", user="root", password="Narenguru2007", database="new_project"
+    host="localhost", user="root", password="Naren", database="new_project"
 )
 
 cur = con.cursor()
@@ -106,7 +106,10 @@ def admin():
     astea = adtab.add("Assign class For teacher")
 
     # display teacher
-    tree1 = ttk.Treeview(distea, height=600)
+    s = ttk.Style(distea)
+    s.theme_use('clam')
+    s.configure('Treeview',rowheight=40)
+    tree1 = ttk.Treeview(distea, height=600,show='headings')
     tree1.pack(padx=10, pady=10)
     tree1["columns"] = ("tid", "tname", "classt", "tclasses")
     tree1.column("#0", width=0, anchor="center")
@@ -158,8 +161,11 @@ def admin():
     combo.pack(pady=10)
     combo.bind("<<ComboboxSelected>>", changestub)
     combo.set("All")
+    s = ttk.Style(distub)
+    s.theme_use('clam')
+    s.configure('Treeview',rowheight=40)
 
-    tree2 = ttk.Treeview(distub, height=600)
+    tree2 = ttk.Treeview(distub,show='headings', height=600)
     tree2.pack(padx=10, pady=10)
     tree2["columns"] = ("sid", "sname", "sclass", "dob", "fname", "mname")
     tree2.column("#0", width=0, anchor="center")
@@ -183,75 +189,6 @@ def admin():
 
     for i in range(len(sdata)):
         tree2.insert(parent="", index="end", iid=i, values=sdata[i])  # type: ignore
-
-    # display student mark
-    '''def getstum(x):
-        cur = con.cursor()
-
-        if x == "All":
-            sql = f"select * from studentmark"
-        else:
-            sql = f"select * from studentmark where sclass = '{x}'"
-        cur.execute(sql)
-        return cur.fetchall()
-
-    def changestum(event):
-        for j in tree3.get_children():
-            tree3.delete(j)
-        inval = getstum(combo.get())
-        for i in range(len(inval)):
-            tree3.insert(parent="", index="end", iid=i, values=inval[i])  # type: ignore
-
-    val1 = getval()
-    val1.insert(0, "All")
-    combo1 = ttk.Combobox(distum, width=20, values=val, state="readonly")
-    combo1.pack(pady=10)
-    combo1.bind("<<ComboboxSelected>>", changestum)
-    combo1.set("All")
-
-    tree3 = ttk.Treeview(distum, height=600)
-    tree3.pack(padx=10, pady=10)
-    tree3["columns"] = (
-        "sid",
-        "sclass",
-        "ut1",
-        "ut2",
-        "ut3",
-        "quarterly",
-        "ut4",
-        "halfyearly",
-        "ut5",
-        "annualexam",
-    )
-    tree3.column("#0", width=-1, anchor="center")
-    tree3.column("sid", width=100, anchor="center")
-    tree3.column("sclass", width=100, anchor="center")
-    tree3.column("ut1", width=100, anchor="center")
-    tree3.column("ut2", width=100, anchor="center")
-    tree3.column("ut3", width=100, anchor="center")
-    tree3.column("quarterly", width=100, anchor="center")
-    tree3.column("ut4", width=100, anchor="center")
-    tree3.column("halfyearly", width=100, anchor="center")
-    tree3.column("ut5", width=100, anchor="center")
-    tree3.column("annualexam", width=100, anchor="center")
-
-    tree3.heading("sid", text="Admission_Id")
-    tree3.heading("sclass", text="Student_Class")
-    tree3.heading("ut1", text="UT-1")
-    tree3.heading("ut2", text="UT-2")
-    tree3.heading("ut3", text="UT-3")
-    tree3.heading("quarterly", text="Quarterly")
-    tree3.heading("ut4", text="UT-4")
-    tree3.heading("halfyearly", text="Half-Yearly")
-    tree3.heading("ut5", text="UT-5")
-    tree3.heading("annualexam", text="Annual")
-
-    q3 = "select * from studentmark"
-    cur.execute(q3)
-    smdata = cur.fetchall()
-
-    for i in range(len(smdata)):
-        tree3.insert(parent="", index="end", iid=i, values=smdata[i])  # type: ignore'''
 
     # add student bio
     inframe = tk.CTkFrame(adstub)
@@ -315,6 +252,30 @@ def admin():
     )
     spassn.grid(row=3, column=1)
 
+    def submite(event):
+        admn = adiden.get() or "NULL"
+        name = namen.get() or "NULL"
+        class_sec = classcombo.get() or "NULL"
+        dob = doben.get() or "NULL"
+        fname = fnamen.get() or "NUll"
+        mname = mnamen.get() or "NULL"
+        spass = spassn.get()
+        cur = con.cursor()
+        sql = f"insert into studentbio values({admn},'{spass}','{name}','{class_sec}','{dob}','{fname}','{mname}')"
+        cur.execute(sql)
+        con.commit()
+        sql2 =f"insert into studentmark(sid,sclass) values({admn},'{class_sec}')"
+        cur.execute(sql2)
+        con.commit()
+        adiden.delete(0, END)
+        namen.delete(0, END)
+        doben.delete(0, END)
+        fnamen.delete(0, END)
+        mnamen.delete(0, END)
+        spassn.delete(0, END)
+        tkmb.showinfo("Insert", "Inserted Succesfully")
+        adtab.set("Display Student Bio")
+        changein()
     def submit():
         admn = adiden.get() or "NULL"
         name = namen.get() or "NULL"
@@ -341,6 +302,7 @@ def admin():
         changein()
 
     getbut = tk.CTkButton(adstub, text="Submit", command=submit)
+    getbut.bind('<Return>',submite)
     getbut.pack(pady=30)
 
     # add teacher
@@ -418,9 +380,27 @@ def admin():
         adtab.set("Display Teacher")
 
         tchangein()
+    def tsubmite(event):
+        tid = tiden.get() or "NULL"
+        tname = tnamen.get() or "NULL"
+        classt = tclasscombo.get() or "NULL"
+        hanclass = hanen.get() or "NULL"
+        tpass1 = tpassen.get() or "NUll"
+        cur = con.cursor()
+        sql = f"insert into teacher values({tid},'{tpass1}','{tname}','{classt}','{hanclass}')"
+        cur.execute(sql)
+        con.commit()
+        tiden.delete(0, END)
+        tnamen.delete(0, END)
+        hanen.delete(0, END)
+        tpassen.delete(0, END)
+        tkmb.showinfo("Insert", "Inserted Succesfully")
+        adtab.set("Display Teacher")
 
-    getbut = tk.CTkButton(adtea, text="Submit", command=tsubmit)
-    getbut.pack(pady=30)
+        tchangein()
+    getbut2 = tk.CTkButton(adtea, text="Submit", command=tsubmit)
+    getbut2.bind('<Return>',tsubmite)
+    getbut2.pack(pady=30)
 
     # assign a class for teacher
     atframe = tk.CTkFrame(astea)
@@ -454,9 +434,22 @@ def admin():
         tkmb.showinfo("Update", "Updated Succesfully")
         adtab.set("Display Teacher")
         tchangein()
-
-    getbut2 = tk.CTkButton(astea, text="Submit", command=updateclass)
-    getbut2.pack(pady=30)
+    def updateclasse(event):
+        astid = astiden.get()
+        asclass = asen.get()
+        asclass = "," + asclass
+        q = f"update teacher set handling_classes = concat(handling_classes,'{asclass}') where tid={astid}"
+        cur = con.cursor()
+        cur.execute(q)
+        con.commit()
+        astiden.delete(0, END)
+        asen.delete(0, END)
+        tkmb.showinfo("Update", "Updated Succesfully")
+        adtab.set("Display Teacher")
+        tchangein()
+    getbut3 = tk.CTkButton(astea, text="Submit", command=updateclass)
+    getbut3.bind('<Return>',updateclasse)
+    getbut3.pack(pady=30)
 
     adwin.mainloop()
 
@@ -476,7 +469,19 @@ def teacher(tid):
             donewindow_messagebox = tkmb.showinfo(
                 "Done!", "The record has been updated!"
             )
+        def change_markse(event):
+            examval = exam.get()
+            entryval = entry.get()
+            markval = mark.get()
 
+            q = "update studentmark set {} = {} where sid = {}".format(
+                examval, markval, entryval
+            )
+            cur.execute(q)
+            con.commit()
+            donewindow_messagebox = tkmb.showinfo(
+                "Done!", "The record has been updated!"
+            )
         mksadd = tk.CTk()
         mksadd.geometry("800x600")
         mksadd.resizable(width=False, height=False)
@@ -585,6 +590,7 @@ def teacher(tid):
         dobut = tk.CTkButton(
             mksadd, width=200, text="Click to do the changes", command=change_marks
         )
+        dobut.bind('<Return>',change_markse)
         dobut.pack()
 
         mksadd.mainloop()
@@ -781,7 +787,12 @@ def student(sid):
         cur.execute(q)
         dat12 = cur.fetchall()
         newin = tk.CTk()
+        newin.title('Display Marks')
         newin.geometry("700x700")
+        s = ttk.Style(newin)
+        s.theme_use('clam')
+        s.configure('Treeview',rowheight=40)
+    
         tree3 = ttk.Treeview(newin, show="headings", height=7)
         tree3.pack(padx=20, pady=30)
         tree3["columns"] = (
